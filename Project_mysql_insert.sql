462176e1c03c6004d3413ec7ef889db7efa0b12c
Kyle Maitland <Kmaitland@madisoncollege.edu>
2018-10-09 21:34:09 -0500

    Removed incorrect insert statement

--- Project_mysql_insert.sql
+++ Project_mysql_insert.sql
@@ -1,8 +1,1 @@
-INSERT INTO User VALUES (1,'Joe','Coyne','jcoyne','supersecret1','1964-04-01')
-  ,(2,'Fred','Hensen','fhensen','supersecret2','1988-05-08')
-  ,(3,'Barney','Curry','bcurry','supersecret3','1947-11-11')
-  ,(4,'Karen','Mack','kmack','supersecret4','1986-07-08')
-  ,(5,'Dianne','Klein','dklein','supersecret5','1991-09-22')
-  ,(6,'Dawn','Tillman','dtillman','supersecret6','1979-08-30');
-
 INSERT INTO User VALUE (1, 'SoundSide', 'holdem09', 'Veteran', 5);
