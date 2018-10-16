823f57a6479f8001c2c3742343892ff6f3e3f48d
Kyle Maitland <Kmaitland@madisoncollege.edu>
2018-10-15 22:31:26 -0500

    Created test directories and began work on the EquipmentDaoTest

---
+++ src/test/java/edu/matc/persistence/EquipmentDaoTest.java
@@ -0,0 +1,80 @@
+package edu.matc.persistence;
+
+import edu.matc.entity.Equipment;
+import edu.matc.entity.Weapons;
+import edu.matc.testutils.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class EquipmentDaoTest {
+
+    EquipmentDao equipmentDao;
+    WeaponsDao weaponsDao;
+
+    @BeforeEach
+    void setUp() {
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+        database.runSQL("insertdb.sql");
+
+        equipmentDao = new EquipmentDao();
+        weaponsDao = new WeaponsDao();
+    }
+
+    @Test
+    void getByIdSuccess() {
+        Equipment armorSet = equipmentDao.getById(1);
+
+        Weapons weapon = weaponsDao.getById(armorSet.getWeaponID());
+
+        assertEquals("Xeno Insect Glaive", weapon.getName());
+    }
+
+    @Test
+    void getAllSuccess() {
+        List<Equipment> equipmentList = equipmentDao.getAll();
+        assertEquals(4, equipmentList.size());
+    }
+
+    @Test
+    void insertSuccess() {
+        Equipment newEquipment = new Equipment(1,1,1,1,1,1);
+        equipmentDao.insert(newEquipment);
+
+        newEquipment = equipmentDao.getById(5);
+
+        Weapons weapon = weaponsDao.getById(newEquipment.getWeaponID());
+
+        assertEquals("Iron SnS", weapon.getName());
+    }
+
+    @Test
+    void deleteSuccess() {
+        equipmentDao.delete(equipmentDao.getById(2));
+        assertNull(equipmentDao.getById(2));
+    }
+
+    @Test
+    void updateSuccess() {
+        Equipment armorSetUpdate = equipmentDao.getById(4);
+
+        armorSetUpdate.setHeadID(3);
+        armorSetUpdate.setChestID(3);
+        armorSetUpdate.setArmsID(3);
+        armorSetUpdate.setWaistID(3);
+        armorSetUpdate.setLegsID(3);
+
+        equipmentDao.saveOrUpdate(armorSetUpdate);
+
+        Equipment armorSetAfterUpdate = equipmentDao.getById(4);
+
+        assertEquals(armorSetUpdate.getChestID(), armorSetAfterUpdate.getChestID());
+        assertEquals(armorSetUpdate.getArmsID(), armorSetAfterUpdate.getArmsID());
+        assertEquals(armorSetUpdate.getLegsID(), armorSetAfterUpdate.getLegsID());
+    }
+}
