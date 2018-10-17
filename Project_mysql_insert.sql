abf4aa06f7963ff1853b5fb2c673ef96f5d6de6c
Kyle Maitland <Kmaitland@madisoncollege.edu>
2018-10-16 23:37:15 -0500

    Touched up Sql files

--- Project_mysql_insert.sql
+++ Project_mysql_insert.sql
@@ -1,38 +1,32 @@
-INSERT INTO User VALUE (1, 'SoundSide', 'holdem09', 'Veteran', 1);
-INSERT INTO User VALUE (2, 'Fred', 'cummin90', 'Novice', 2);
-INSERT INTO User VALUE (3, 'Whale', 'disarmed', 'Novice', 3);
-INSERT INTO User VALUE (4, 'Silent3mpty', 'holdem09', 'Veteran', 4);
-
-INSERT INTO Equipment VALUE (1, 3, 3, 3, 3, 3, 3);
-INSERT INTO Equipment VALUE (2, 2, 1, 1, 1, 1, 1);
-INSERT INTO Equipment VALUE (3, 1, 2, 2, 2, 2, 2);
-INSERT INTO Equipment VALUE (4, 4, 4, 4, 4, 4, 4);
-
 INSERT INTO Weapons VALUE (1, 'Iron SnS');
 INSERT INTO Heads VALUE (1, 'Leather Helmet');
 INSERT INTO Chests VALUE (1, 'Leather Chest');
 INSERT INTO Arms VALUE (1, 'Leather Gauntlets');
 INSERT INTO Waists VALUE (1, 'Leather Guards');
 INSERT INTO Legs VALUE (1, 'Leather Boots');
-
 INSERT INTO Weapons VALUE (2, 'Iron Insect Glaive');
 INSERT INTO Heads VALUE (2, 'Iron Helmet');
 INSERT INTO Chests VALUE (2, 'Iron Chest');
 INSERT INTO Arms VALUE (2, 'Iron Gauntlets');
 INSERT INTO Waists VALUE (2, 'Iron Guards');
 INSERT INTO Legs VALUE (2, 'Iron Boots');
-
 INSERT INTO Weapons VALUE (3, 'Xeno Insect Glaive');
 INSERT INTO Heads VALUE (3, 'XenoJiva Helmet');
 INSERT INTO Chests VALUE (3, 'XenoJiva Chest');
 INSERT INTO Arms VALUE (3, 'XenoJiva Gauntlets');
 INSERT INTO Waists VALUE (3, 'XenoJiva Guards');
 INSERT INTO Legs VALUE (3, 'XenoJiva Boots');
-
 INSERT INTO Weapons VALUE (4, 'Nergergantie Great Sword');
 INSERT INTO Heads VALUE (4, 'Nergergantie Helmet');
 INSERT INTO Chests VALUE (4, 'Nergergantie Chest');
 INSERT INTO Arms VALUE (4, 'Nergergantie Gauntlets');
 INSERT INTO Waists VALUE (4, 'Nergergantie Guards');
 INSERT INTO Legs VALUE (4, 'Nergergantie Boots');
-
+INSERT INTO Equipment VALUE (1, 3, 3, 3, 3, 3, 3);
+INSERT INTO Equipment VALUE (2, 2, 1, 1, 1, 1, 1);
+INSERT INTO Equipment VALUE (3, 1, 2, 2, 2, 2, 2);
+INSERT INTO Equipment VALUE (4, 4, 4, 4, 4, 4, 4);
+INSERT INTO User VALUE (1, 'SoundSide', 'holdem09', 'Veteran', 1);
+INSERT INTO User VALUE (2, 'Fred', 'cummin90', 'Novice', 2);
+INSERT INTO User VALUE (3, 'Whale', 'disarmed', 'Novice', 3);
+INSERT INTO User VALUE (4, 'Silent3mpty', 'holdem09', 'Veteran', 4);
