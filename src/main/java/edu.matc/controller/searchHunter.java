db69a6e8158053e67e84aeb4bd64e96ed8c02bad
Kyle Maitland <Kmaitland@madisoncollege.edu>
2018-10-21 21:36:32 -0500

    Created the search hunter java servlet

---
+++ src/main/java/edu.matc/controller/searchHunter.java
@@ -0,0 +1,41 @@
+package edu.matc.controller;
+
+import edu.matc.persistence.UserDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(
+        urlPatterns = {"/searchHunters"}
+)
+public class searchHunter extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String searchTerm;
+        String searchType;
+
+
+        UserDao userDao = new UserDao();
+
+        if (req.getParameter("searchTerm") != null ||
+                !req.getParameter("searchTerm").equals("")) {
+            searchTerm = req.getParameter("searchTerm");
+            req.setAttribute("hunters", userDao.getUsersByUserName(searchTerm));
+        } else {
+            req.setAttribute("hunters", userDao.getAll());
+        }
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/searchHunters.jsp");
+        dispatcher.forward(req, resp);
+
+    }
+
+}
