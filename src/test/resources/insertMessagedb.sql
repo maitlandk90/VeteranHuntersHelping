47fe2aeef92ca51c1425c59feffa911203ef0981
Kyle Maitland <Kmaitland@madisoncollege.edu>
2018-12-19 11:59:03 -0600

    Got tests working for presentation

--- src/test/resources/insertMessagedb.sql
+++ src/test/resources/insertMessagedb.sql
@@ -1,3 +1,3 @@
-insert into Messages values (1,1,2,'test test');
-insert into Messages values (2,1,3,'Hey be my vet!');
+insert into Messages values (1,1,2,'Lets hunt some elder dragons tonight');
+insert into Messages values (2,1,3,'Can you teach me some glaive techniques');
 insert into Messages values (3,3,1,'Will Do :)');
